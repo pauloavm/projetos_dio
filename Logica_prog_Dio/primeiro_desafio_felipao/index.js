73e5d83db77026630db9f6870a31a8933b010e28
pauloavm <pauloavmunhoz@gmail.com>
2024-05-31 14:08:27 -0300

    Ajuste de variável.

--- Logica_prog_Dio/primeiro_desafio_felipao/index.js
+++ Logica_prog_Dio/primeiro_desafio_felipao/index.js
@@ -1,20 +1,21 @@
-let nomeHeroi = "Guerreiro do Sol";
-let xpHeroi = 7500;
+let nomeHeroi = "TeoPae";
+let xpHeroi = 9368;
+let nivel = "";
 
 if (xpHeroi < 1000) {
-  let nivel = "Ferro";
+  nivel = "Ferro";
 } else if (xpHeroi >= 1001 && xpHeroi <= 12000) {
-  let nivel = "Bronze";
+  nivel = "Bronze";
 } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-  let nivel = "Prata Ouro";
+  nivel = "Prata Ouro";
 } else if (xpHeroi >= 5001 && xpHeroi <= 8000) {
-  let nivel = "Platina Diamante";
+  nivel = "Platina Diamante";
 } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-  let nivel = "Ascendente";
+  nivel = "Ascendente";
 } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-  let nivel = "Imortal";
+  nivel = "Imortal";
 } else {
-  let nivel = "Radiante";
+  nivel = "Radiante";
 }
 
 console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}.`);
