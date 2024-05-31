baaeee915deb7fd5cb51d966bf16db99386d7b1d
pauloavm <pauloavmunhoz@gmail.com>
2024-05-31 17:48:21 -0300

    Update script.js

--- Logica_prog_Dio/logicas_gerais/script.js
+++ Logica_prog_Dio/logicas_gerais/script.js
@@ -7,3 +7,18 @@
 // let numero = "1";
 // console.log(numero === 1);
 
+function calcularXpGanhos(nivelMonstro, dificuldadeBatalha) {
+    return nivelMonstro * dificuldadeBatalha * 100;
+  }
+  
+  // Função principal para executar a simulação
+  function simularBatalha(nivelMonstro, dificuldadeBatalha) {
+    const xpGanhos = calcularXpGanhos(nivelMonstro, dificuldadeBatalha);
+    console.log(`Voce ganhou ${xpGanhos} XP!`);
+  }
+  
+//   // Exemplos de uso
+//   simularBatalha(15, 20); // Espera: Voce ganhou 30000 XP!
+//   simularBatalha(45, 40); // Espera: Voce ganhou 180000 XP!
+//   simularBatalha(41, 38); // Espera: Voce ganhou 155800 XP!
+  
