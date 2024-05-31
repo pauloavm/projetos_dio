640d4cbee0a09bda03f1c7a73f08ab0fd8c8c609
pauloavm <pauloavmunhoz@gmail.com>
2024-05-31 12:38:07 -0300

    Create condicionais.js

---
+++ Logica_prog_Dio/logicas_gerais/condicionais.js
@@ -0,0 +1,11 @@
+let possuiOvos = false;
+let itensComprados = "";
+
+if (possuiOvos) {
+  itensComprados = "Leite";
+} else {
+  console.log("passou na sessão de congelados");
+  itensComprados = "Lasanha congelada";
+}
+
+console.log("item comprado: " + itensComprados);
